771fdc6415cee91eea44a1bc111d7108bb67decc
Zailabidin <sulajmankulovzajlabidin@gmail.com>
2023-02-16 01:35:49 +0700

    bmi calculator

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bmi_calc/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BmiCalculator());
+}
+
+class BmiCalculator extends StatelessWidget {
+  const BmiCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:const HomeScreen(),
+    );
+  }
+}
